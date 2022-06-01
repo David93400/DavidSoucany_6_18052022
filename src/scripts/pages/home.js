c0ced592922b9a2014bb02e1cd5e8e68e968272d
David Soucany <david.soucany@bloomays.com>
2022-06-01 17:44:40 +0200

    set up setTags function and some integration (tag & select)

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -6,9 +6,7 @@ import {
   recipesConstants,
 } from '../utils/helpers';
 import '../../css/home.css';
-recipesConstants.forEach((type) => {
-  console.log(type);
-});
+
 async function getRecipes() {
   const data = await customFetch('./data/recipes.json');
   console.log(data.recipes);
@@ -24,7 +22,6 @@ async function displayRecipes(recipes) {
   });
 }
 const getList = (recipes, type) => {
-  console.log(type);
   const list = recipes.reduce((acc, recipe) => {
     if (type === 'appliance') {
       if (!acc.includes(recipe[type])) {
@@ -52,6 +49,7 @@ const selectUtensilsDisplay = createGenericElement('option', 'Ustensiles');
 
 const searchSection = document.querySelector('.search-section');
 const selectSection = createGenericElement('div', '', 'select-section');
+const tagSection = createGenericElement('div', '', 'tag-section');
 const searchBar = createGenericElement('input', '', 'search-bar', [
   { name: 'type', value: 'text' },
   { name: 'placeholder', value: 'Rechercher une recette' },
@@ -62,22 +60,49 @@ searchSection.appendChild(searchBar);
 const selectIngredients = createGenericElement(
   'select',
   '',
-  'select-ingredients'
+  'select-ingredients',
+  [{ name: 'id', value: 'Ingrédients' }]
 );
-const selectAppareils = createGenericElement('select', '', 'select-appliance');
-const selectUtensils = createGenericElement('select', '', 'select-ustensils');
+const selectAppareils = createGenericElement('select', '', 'select-appliance', [
+  { name: 'id', value: 'Appareils' },
+]);
+const selectUtensils = createGenericElement('select', '', 'select-ustensils', [
+  { name: 'id', value: 'Ustensiles' },
+]);
 
 selectSection.appendChild(selectIngredients);
 selectSection.appendChild(selectAppareils);
 selectSection.appendChild(selectUtensils);
+searchSection.appendChild(tagSection);
 searchSection.appendChild(selectSection);
 selectIngredients.appendChild(selectIngredientsDisplay);
 selectAppareils.appendChild(selectAppareilsDisplay);
 selectUtensils.appendChild(selectUtensilsDisplay);
+const selects = [selectIngredients, selectAppareils, selectUtensils];
+
+const setTags = (selectType) => {
+  selectType.addEventListener('change', (e) => {
+    const value = e.target.value;
+    if (value === selectType.id) {
+      return;
+    }
+    const closeTag = createGenericElement('div', 'X', 'close-tag');
+    const tag = createGenericElement(
+      'div',
+      '',
+      `${selectType.className}-tag tag`,
+      [{ name: 'id', value: `${e.target.value}` }]
+    );
+    tag.appendChild(closeTag);
+    tagSection.appendChild(tag);
+    tag.textContent = value;
+  });
+};
 
 export default async function init() {
   const { recipes } = await getRecipes();
   recipesConstants.map((type) => getList(recipes, type));
+  selects.map((selectType) => setTags(selectType));
   displayRecipes(recipes);
 }
 
